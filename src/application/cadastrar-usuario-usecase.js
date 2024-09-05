cb5ee918eb1179fabffc8abc03f66daefa9bd123
wigor <wigor21@hotmail.com>
2024-09-04 23:13:32 -0300

    feat: adicionar o conceito de fluxo de requisicao e criar o cadastrarUsuariosUseCase

---
+++ src/application/cadastrar-usuario-usecase.js
@@ -0,0 +1,7 @@
+module.exports = function cadastrarUsuarioUseCase() {
+    return async function ({ nome_completo, cpf, telefone, endereco, email }) {
+        await usuarioRepository.cadastrar({
+            nome_completo, cpf, telefone, endereco, email
+        })
+    }
+}
